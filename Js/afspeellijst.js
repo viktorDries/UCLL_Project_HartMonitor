c92f9f87fdaa7230a5d1feead392fd376a5752c0
LemmensBram <127494143+LemmensBram@users.noreply.github.com>
2023-12-17 16:17:01 +0100

    ferfsd

--- Js/afspeellijst.js
+++ Js/afspeellijst.js
@@ -45,10 +45,9 @@ template.innerHTML = /*html*/`
 class CustomAfspeellijst extends HTMLElement {
     constructor() {
         super();
-
         this.attachShadow({"mode": "open"});
         this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
 }
 
-customElements.define('custom-afspeellijst', customAfspeellijst);
+customElements.define('custom-afspeellijst', CustomAfspeellijst);

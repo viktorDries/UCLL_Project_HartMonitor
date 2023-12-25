750ecc56e75d1823254e3dd96e3d16b5f55f8343
LemmensBram <127494143+LemmensBram@users.noreply.github.com>
2023-12-25 16:50:03 +0100

    bpm value + average

--- Js/app.js
+++ Js/app.js
@@ -3,6 +3,7 @@ import "./muziek.js"
 import "./hartslag.js"
 import "./afspeellijst.js"
 import "./bpm.js"
+import "./bpm-display.js"
 
 const template = document.createElement("template")
 template.innerHTML = /*html*/`
@@ -11,12 +12,13 @@ template.innerHTML = /*html*/`
     <my-counter></my-counter>
     <chart-element></chart-element>
     <playlist-component songs='[
-        {"title": "Song 1", "artist": "Artist 1", "duration": "0:05", "bpm": 120, "playing": false},
-        {"title": "Song 2", "artist": "Artist 2", "duration": "0:07", "bpm": 140, "playing": false},
-        {"title": "Song 3", "artist": "Artist 3", "duration": "0:03", "bpm": 100, "playing": false},
-        {"title": "Song 4", "artist": "Artist 4", "duration": "0:10", "bpm": 90, "playing": false},
-        {"title": "Song 5", "artist": "Artist 5", "duration": "0:04", "bpm": 110, "playing": false}
+        {"title": "Song 1", "artist": "Artist 1", "duration": "0:01", "bpm": 120, "playing": false},
+        {"title": "Song 2", "artist": "Artist 2", "duration": "0:01", "bpm": 140, "playing": false},
+        {"title": "Song 3", "artist": "Artist 3", "duration": "0:01", "bpm": 100, "playing": false},
+        {"title": "Song 4", "artist": "Artist 4", "duration": "0:01", "bpm": 90, "playing": false},
+        {"title": "Song 5", "artist": "Artist 5", "duration": "0:01", "bpm": 110, "playing": false}
       ]'></playlist-component>
+      <bpm-display></bpm-display bpmValue="80">
 `
 
 class app extends HTMLElement {
---
+++ Js/bpm-display.js
@@ -0,0 +1,98 @@
+// Define the BPM Display web component
+class BPMDisplay extends HTMLElement {
+    constructor() {
+      super();
+
+      // Create a shadow DOM
+      this.attachShadow({ mode: 'open' });
+
+      // Initial BPM values
+      this.bpm = 0;
+      this.averageBPM = 0;
+
+      // Render the component
+      this.render();
+
+      // Check for changes in attributes
+      const observer = new MutationObserver(() => {
+        this.updateBPM(this.getAttribute('bpmvalue'));
+      });
+
+      observer.observe(this, { attributes: true, attributeFilter: ['bpmvalue'] });
+
+      // Simulate BPM changes (replace this with your actual logic to get BPM)
+      setInterval(() => {
+        const newBPM = Math.floor(Math.random() * 150) + 50;
+        this.updateBPM(newBPM);
+        this.updateAverageBPM(newBPM);
+      }, 2000);
+    }
+
+    // Method to update BPM value
+    updateBPM(newBPM) {
+      if (!isNaN(newBPM)) {
+        this.bpm = newBPM;
+        this.render();
+      }
+    }
+
+    // Method to update average BPM value
+    updateAverageBPM(newBPM) {
+      // Simple moving average calculation (replace with your own logic)
+      const alpha = 0.2; // Smoothing factor
+      this.averageBPM = alpha * newBPM + (1 - alpha) * this.averageBPM;
+      this.render();
+    }
+
+    // Method to render the component
+    render() {
+      // Use the template content for rendering
+      const content = document.importNode(template.content, true);
+
+      // Update BPM values in the content
+      content.querySelector('.bpm-value').textContent = this.bpm.toFixed(2);
+      content.querySelector('.average-bpm-value').textContent = this.averageBPM.toFixed(2);
+
+      // Append content to the shadow DOM
+      this.shadowRoot.innerHTML = '';
+      this.shadowRoot.appendChild(content);
+    }
+  }
+
+  // Define the custom element
+  customElements.define('bpm-display', BPMDisplay);
+
+  // Define the template outside of the component
+  const template = document.createElement('template');
+  template.innerHTML = `
+    <style>
+      /* Use :host to apply styles within the shadow DOM */
+      :host .heart {
+        color: red;
+        animation: heartbeat 1s infinite alternate;
+      }
+
+      @keyframes heartbeat {
+        from {
+          transform: scale(1);
+        }
+
+        to {
+          transform: scale(1.2);
+        }
+      }
+
+      div {
+        /* Add any other styles you want for the container */
+      }
+
+      /* Add any other styles as needed */
+    </style>
+    <div>
+      <span>BPM: <span class="bpm-value"></span></span>
+      <span class="heart">❤️</span>
+      <br>
+      <span>Average BPM: <span class="average-bpm-value"></span></span>
+      <span class="heart">❤️</span>
+    </div>
+  `;
